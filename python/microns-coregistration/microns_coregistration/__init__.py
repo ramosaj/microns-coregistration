f1832a791aaabd1b19ce6cef91a094a47f869b8d
spapa013 <stelios@spapa.us>
2021-11-15 16:30:35 -0600

    updates package __init__ for version tracking

--- python/microns-coregistration/microns_coregistration/__init__.py
+++ python/microns-coregistration/microns_coregistration/__init__.py
@@ -1,10 +1,3 @@
-import warnings
-from microns_utils import config_utils
+from microns_utils import version_utils
 
-_repo = 'microns-coregistration'
-_package = _repo
-
-try:
-    __version__ = config_utils.get_package_version(repo=_repo, package=_package)
-except:
-    warnings.warn('Package version not able to be determined.')
+__version__ = version_utils.get_package_version(repo='microns-coregistration', package='microns-coregistration')
