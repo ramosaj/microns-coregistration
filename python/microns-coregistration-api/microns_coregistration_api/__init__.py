0abd2885c69cb8e9dba8f31c98d94a2a2fc01e0f
spapa013 <stelios@spapa.us>
2021-11-15 12:18:17 -0600

    add try/ catch for version

--- python/microns-coregistration-api/microns_coregistration_api/__init__.py
+++ python/microns-coregistration-api/microns_coregistration_api/__init__.py
@@ -1,6 +1,10 @@
+import warnings
 from microns_utils import config_utils
 
 _repo = 'microns-coregistration'
 _package = 'microns-coregistration-api'
 
-__version__ = config_utils.get_package_version(repo=_repo, package=_package)
+try:
+    __version__ = config_utils.get_package_version(repo=_repo, package=_package)
+except:
+    warnings.warn('Package version not able to be determined.')
