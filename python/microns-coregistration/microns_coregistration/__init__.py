4f8184c5d6db541bc9b2fd420c108f9bfb89d1c8
spapa013 <stelios@spapa.us>
2021-11-14 21:28:23 -0600

    updates directory and adds __init__.py

--- python/microns-coregistration/microns_coregistration/__init__.py
+++ python/microns-coregistration/microns_coregistration/__init__.py
@@ -1,6 +1,6 @@
 from microns_utils import config_utils
 
 _repo = 'microns-coregistration'
-_package = 'microns-coregistration-api'
+_package = _repo
 
 __version__ = config_utils.get_package_version(repo=_repo, package=_package)
