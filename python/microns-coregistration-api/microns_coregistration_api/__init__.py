cb2d8d1ca14c86ace2ae206c97275adb9c518094
spapa013 <stelios@spapa.us>
2021-11-16 10:05:59 -0600

    updates version control

--- python/microns-coregistration-api/microns_coregistration_api/__init__.py
+++ python/microns-coregistration-api/microns_coregistration_api/__init__.py
@@ -1,3 +1,14 @@
 from microns_utils import version_utils
 
-__version__ = version_utils.get_package_version(repo='microns-coregistration', package='microns-coregistration-api')
+__version__ = version_utils.get_package_version(
+    package='microns-coregistration-api', 
+    check_if_latest=True, 
+    check_if_latest_kwargs=dict(
+        repo='microns-coregistration', 
+        user='cajal', 
+        branch='main', 
+        source='commit', 
+        path_to_version_file='python/version.py', 
+        tag=None
+    )
+)
