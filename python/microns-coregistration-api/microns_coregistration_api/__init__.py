f46953af8a8b6ffbf2f1e2d7a47117a80288562a
spapa013 <stelios@spapa.us>
2021-11-17 13:45:19 -0600

    update version check to tag

--- python/microns-coregistration-api/microns_coregistration_api/__init__.py
+++ python/microns-coregistration-api/microns_coregistration_api/__init__.py
@@ -4,11 +4,8 @@ __version__ = version_utils.get_package_version(
     package='microns-coregistration-api', 
     check_if_latest=True, 
     check_if_latest_kwargs=dict(
+        owner='cajal', 
         repo='microns-coregistration', 
-        user='cajal', 
-        branch='main', 
-        source='commit', 
-        path_to_version_file='python/version.py', 
-        tag=None
+        source='tag', 
     )
 )
