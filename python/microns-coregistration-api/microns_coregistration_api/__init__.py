cb7ebc0f14b54c8daf80b9f4cba5b6f7c496de53
spapa013 <stelios@spapa.us>
2021-11-18 09:02:36 -0600

    add check_latest_version to __init__

--- python/microns-coregistration-api/microns_coregistration_api/__init__.py
+++ python/microns-coregistration-api/microns_coregistration_api/__init__.py
@@ -1,6 +1,6 @@
 from microns_utils import version_utils
 
-__version__ = version_utils.get_package_version(
+__version__ = version_utils.check_package_version(
     package='microns-coregistration-api', 
     check_if_latest=True, 
     check_if_latest_kwargs=dict(
@@ -9,3 +9,9 @@ __version__ = version_utils.get_package_version(
         source='tag', 
     )
 )
+
+def check_latest_version_from_github(owner='cajal', repo='microns-coregistration', source='tag', warn=True):
+    """
+    Wrapper for :func:`~microns_utils.version_utils.check_latest_version_from_github`
+    """
+    return version_utils.check_latest_version_from_github(owner=owner, repo=repo, source=source, warn=warn)
